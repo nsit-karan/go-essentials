67abea24576e5909fdabf690bbfe78d80d66f9ce
karan arora <nsit_karan@yahoo.com>
2025-04-06 18:22:55 +0530

    assignment1 for module1

---
+++ module1/assignments/assignment1.go
@@ -0,0 +1,23 @@
+package assignments
+
+import "fmt"
+
+func CreateMap() map[string]int {
+	var mapas map[string]int = map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+	}
+
+	return mapas
+}
+
+func DeleteMap(mapArg map[string]int) {
+	for key, value := range mapArg {
+		fmt.Printf("Deleting entry with key : %s, value : %d\n",
+			key, value)
+		delete(mapArg, key)
+	}
+}
