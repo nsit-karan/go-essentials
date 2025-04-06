a0ad4edbb60be75a6a586ef671342c4601855ba7
karan arora <nsit_karan@yahoo.com>
2025-04-06 15:01:00 +0530

    map example continued

--- module1/datastructures/maps.go
+++ module1/datastructures/maps.go
@@ -26,4 +26,26 @@ func basicMaps() {
 	}
 	fmt.Println(map3)
 
+	// check for existence of a key
+	// accessing a map with a key returns 2 values :
+	// the actual value and a boolean to specify if the value was
+	// present or not
+	value3, ok3 := map3[3]
+	value4, ok4 := map3[4]
+
+	if ok3 {
+		fmt.Printf("key:%d was found in the above map, value : %s\n",
+			3, value3)
+	} else if ok4 {
+		fmt.Printf("key:%d was found in the above map, value : %s\n",
+			4, value4)
+	} else {
+		fmt.Println("Neither 3 nor 4 was found")
+	}
+
+	// delete an entry from a map
+	delete(map3, 3)
+	fmt.Println("Content of map3 post deletion")
+	fmt.Println(map3)
+
 }
